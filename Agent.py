00d2499309dbb2e5e80279d61e36a9c88e644de1
samblyth995 <109956274+samblyth995@users.noreply.github.com>
2024-02-15 10:24:20 +0000

    updated from video part 3
    Only plotting and model to implement in part 4

--- Agent.py
+++ Agent.py
@@ -15,23 +15,63 @@ class Agent:
         self.epsilon=0 # controls randomness
         self.gamma =0  # discount rate
         self.memory =deque(maxlen=MAX_MEMORY) #call pop left for us
-        # TO DO model, trainer
+        self.model= None # to do
+        self.trainer=None #to do
 
 
-    def get_state (self, game):
+    def get_state (self, startGame):
         pass
+        #bring in code for discard card
+        #bring in code for the hand
+        #state=[list of 11 states
+            #have to pick_up
+            #won
+            #lost
+            #wild_card avaialble
+            # check for playable_card against played card]
+        state=[
+
+        ]
+        #convert list to numpy array
+        return np.array(state, dtype=int)
 
     def remember(self, state, action, reward, next_state, done):
-        pass
+        self.memory.append((state, action, reward, next_state, done))# pop left if max memory is reached
 
     def train_long_memory(self):
-        pass
+        if len(self.memory)> BATCH_SIZE:
+            mini_sample= random.sample(self.memory, BATCH_SIZE)# returns a list of tuples
+        else:
+            mini_sample=self.memory
+        #use python built in zip function to group together the states, actions, rewards etc
+        #could also have been done with a for loop
+        states, actions, rewards, next_states, dones = zip(*mini_sample) 
+        self.trainer.train_step(states, actions, rewards, next_states, dones)
+
 
     def train_short_memory(self, state, action, reward, next_state, done):
-        pass
+        self.trainer.train_step(state, action, reward, next_state, done)
+
+
 
     def get_action(self, state):
-        pass
+    #random moves:tradeoff between exploration (PC random)/exploitation( AI learning) - we use epsilon
+        self.epsilon = 80- self.n_games
+        final_move=[0,0,0]
+        if random.randint(0,200)< self.epsilon:
+            move= random.randint(0,2)# 2 is included
+            final_move[move]=1
+    #the more games, the smaller epsilon will get. the smaller the epsilon, the less chance randint willl be less than epsilon
+    #it can get to negative numbers which means, the moves are no longer random
+        else:
+        #we want a move based on our model. It wats to predict based on 1 state (state0)
+        #convert state to a tensor
+            state0=torch.tensor(state, dtype=torch.float)
+            prediction =self.model.predict(state0)
+        #convert from raw number to 1 hot encoded value
+            move =torch.argmax(prediction).item()
+            final_move[move]=1
+        return final_move    
 
 def train():
     plot_scores=[]
@@ -42,13 +82,13 @@ def train():
     #game = SnakeGameAI()
     while True:
         #get the old state
-        state_old = agent.get_state(game)
+        state_old = agent.get_state(startGame)
         #get move
         final_move =agent.get_action(state_old)
 
         #perform move and get new state
-        reward, done, score = game.play_step(final_move)
-        state_new = agent.get_state(game)
+        reward, done, score = startGame.play_step(final_move)
+        state_new = agent.get_state(startGame)
 
         #train short memory
         agent.train_short_memory(state_old, final_move, reward, state_new, done)
@@ -58,7 +98,7 @@ def train():
 
         if done:
             #train the long memory, plot results
-            game.reset()
+            startGame.reset()
             agent.n_games=+1
             agent.train_long_memory()
 
